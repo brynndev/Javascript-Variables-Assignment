77dd6ccbba9377f334c037f1cf1c3889174feab6
Brynn <devlyn.taylor1998@gmail.com>
2026-09-04 20:10:32 -0500

    javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+// Global variable
+let name = "Brynn";
+
+// Block variable
+{
+    let age = 28;
+    console.log(age);
+}
+
+// Mathematical operations
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+// String variable
+let favoriteColor = "Pink"; // This is my favorite color
+console.log(favoriteColor);
+
+// Object variable using const
+const person = {
+    name: "Brynn",
+    age: 28,
+    favoriteColor: "Pink"
+};
+
+// Log the global variable
+console.log(name);
+
+// Log the object
+console.log(person);
